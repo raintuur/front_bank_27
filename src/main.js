6ea2265e40b46b2a84bfed9565568b97ce0c78ab
Rain Tüür <rain.tuur@gmail.com>
2023-01-15 22:31:15 +0200

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueAxios from "vue-axios";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.js"
 
+// font awesome stuff
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret, fas, faR } from '@fortawesome/free-solid-svg-icons'
+
+// font awesome stuff
+library.add(faUserSecret, fas, faR)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+
+Vue.use(VueAxios, axios);
 Vue.config.productionTip = false
 
 new Vue({
