f3e8ef1f23b67f039b47b3d6cd36f9b3840ca3cc
Rain Tüür <rain.tuur@gmail.com>
2023-01-17 17:36:57 +0200

    Cars view added WIP

---
+++ src/views/CarsView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+
+    <div class="row justify-content-center">
+
+      {{carMake}} + {{carModel}}
+      <br>
+      <div class="col col-2">
+        <input v-model="carMake" type="text" class="form-control" placeholder="Auto mark" aria-label="First name">
+      </div>
+      <div class="col col-2">
+        <input v-model="carModel" type="text" class="form-control" placeholder="Auto mudel" aria-label="Last name">
+      </div>
+
+    </div>
+
+  </div>
+
+</template>
+
+
+<script>
+export default {
+  name: 'CarsView',
+  data: function () {
+    return {
+      carMake: '',
+      carModel: ''
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
