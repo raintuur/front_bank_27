80b5c4a9be03b931d72fea4895991dc1ab4dff71
SILVER <kurriksilver@gmail.com>
2023-01-17 16:48:54 +0200

    Vue.js page

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import CarsView from '@/views/CarsView.vue';
+import AboutView from "@/views/AboutView.vue";
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    {
+        path: '/',
+        name: 'homeRoute',
+        component: HomeView
+    },
+    {
+        path: '/about',
+        name: 'aboutRoute',
+        component: AboutView
+
+    },
+    {
+        path: '/cars',
+        name: 'carsRoute',
+        component: CarsView
     }
-  }
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 export default router
---
+++ src/views/CarsView.vue
@@ -0,0 +1,35 @@
+<template>
+
+  <div>
+    {{carMake}} + {{carModel}}
+    <br>
+    <div class="row justify-content-center">
+      <div class="col col-1">
+        <input v-model="carMake" type="text" class="form-control" placeholder="Auto mark" aria-label="AutoMark">
+      </div>
+      <div class="col col-1">
+        <input v-model="carModel" type="text" class="form-control" placeholder="Auto mudel" aria-label="AutoMudel">
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'CarsView',
+  data: function () {
+    return {
+      carMake: 'Audi',
+      carModel: 'A7'
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
