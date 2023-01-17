96bc9c1b9b675f43a30a6b602f377e94bdf47822
Valeria <valeria.metsallik@gmail.com>
2023-01-17 20:49:20 +0200

    17.01.23

---
+++ src/views/CarsView.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+  <div class="row justify-content-center">
+
+
+    <br>
+    <div class="col col-2">
+      <input v-model="carMake" type="text" class="form-control" placeholder="Auto mark" aria-label="Username">
+    </div>
+
+    <div class="col col-2">
+      <input v-model="carModel" type="text" class="form-control" placeholder="Auto number" aria-label="Username">
+    </div>
+
+  </div>
+
+</div>
+</template>
+
+
+
+
+<script>
+export default {
+  name: 'CarsView',
+  data: function (){
+    return {
+      carMake: '',
+      carModel: '',
+    }
+  }
+  methods: {
+
+  }
+}
+</script>
