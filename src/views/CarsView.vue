b20af9e0778748f9e0c25823d5cc39b669cb3d45
Valter Aro <valteraro@gmail.com>
2023-01-17 16:49:06 +0200

    Initial Website commit

---
+++ src/views/CarsView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="row justify-content-center">
+      <div class="col col-2">
+        <input v-model="carMake" type="text" class="form-control" placeholder="Auto mark" aria-label="First name">
+      </div>
+      <div class="col col-2">
+        <input v-model="carModel" type="text" class="form-control" placeholder="Auto mudel" aria-label="Last name">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CarsView',
+  data: function () {
+    return {
+      carMake: '',
+      carModel: ''
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
