ccf914ef33a394f3acd79699f4773d1817cf2a8f
Mark Saaremägi <mark.saaremagi@gmail.com>
2023-01-18 07:09:11 +0200

    Getting on track with the end of the lesson

--- src/views/CarsView.vue
+++ src/views/CarsView.vue
@@ -1,15 +1,33 @@
 <template>
   <div>
-    Tere maailm
+
+
+    <div class="row justify-content-center">
+
+      <div class="col col-2">
+        <input v-model="carMake" type="text" class="form-control" placeholder="AUTOMARK" aria-label="First name">
+      </div>
+
+      <div class="col col-2">
+        <input v-model="carModel" type="text" class="form-control" placeholder="AUTOMUDEL" aria-label="Last name">
+      </div>
+
+    </div>
+
+
+
   </div>
 </template>
 
 <script>
 export default {
-  name: "CarsView"
+  name: 'CarsView',
+  data: function () {
+    return {
+      carMake: 'Audi',
+      carModel: 'A7',
+    }
+  }
 }
 </script>
 
-<style scoped>
-
-</style>
