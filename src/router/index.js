25836e0dddc32e1fc9f7598771e598f398002ba9
Marii <mtelliskivi@gmail.com>
2023-01-18 11:47:21 +0200

    commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import CarsView from "@/views/CarsView.vue";
+import AboutView from "@/views/AboutView.vue";
 
 Vue.use(VueRouter)
 
 const routes = [
+{
+  path: '/cars',
+  name: 'carsRoute',
+  component: CarsView
+},
+
   {
     path: '/',
-    name: 'home',
+    name: 'homeRoute',
     component: HomeView
   },
   {
     path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-    }
+    name: 'aboutRoute',
+    component: AboutView
   }
 ]
 
---
+++ src/views/CarsView.vue
@@ -0,0 +1,59 @@
+<template>
+<div>
+
+<!-- ROW 1 -->
+  <div class="row justify-content-center">
+
+    <!-- COLUMN 1 -->
+    <div class="col-2">
+      <input v-model="car.carMake" type="text" class="form-control" placeholder="Auto mark">
+    </div>
+
+
+    <!-- COLUMN 2 -->
+    <div class="col-2">
+      <input v-model="car.carModel" type="text" class="form-control" placeholder="Auto mudel">
+    </div>
+
+
+    <!-- COLUMN 3 -->
+    <div class="col-3">
+      <button v-on:click="addCar" type="button" class="btn btn-outline-success">Lisa auto</button>
+    </div>
+
+  </div>
+
+</div>
+</template>
+
+
+
+<script>
+import NewCarHeader from "@/components/NewCarHeader.vue";
+
+export default {
+  name: 'CarsView',
+  components: (NewCarHeader),
+  data: function () {
+    return {
+      car: {
+        carMake: '',
+        carModel: '',
+      },
+
+      cars: []
+    }
+  },
+
+  methods: {
+    addCar: function (carMake) {
+      alert("Auto " + this.car.carMake + " " + this.car.carModel + " lisati süsteemi.")
+    }
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
