e8a6af84287f9be3441843ea6b8e98ce11878605
Janeli <janelikurm@gmail.com>
2023-01-18 12:41:12 +0200

    Added button

--- src/views/CarsView.vue
+++ src/views/CarsView.vue
@@ -2,21 +2,7 @@
   <div>
     <NewCarHeader :company-name="companyName"/>
     <!-- ROW 1 -->
-    <div class="row justify-content-center">
-
-      <!-- COLUMN 1 -->
-      <div class="col col-4">
-        <input v-model="car.carMake" type="text" class="form-control" placeholder="Auto mark" aria-label="First name">
-      </div>
-      <!-- COLUMN 2 -->
-      <div class="col col-4">
-        <input v-model="car.carModel" type="text" class="form-control" placeholder="Auto mudel" aria-label="Last name">
-      </div>
-      <!-- COLUMN 3 -->
-      <div class="col col-2">
-        <button v-on:click="addCar" type="button" class="btn btn-outline-success">Lisa auto</button>
-      </div>
-    </div>
+    <NewCarInput :car="car"/>
 
 
   </div>
@@ -25,10 +11,11 @@
 
 <script>
 import NewCarHeader from "@/components/NewCarHeader.vue";
+import NewCarInput from "@/components/car/NewCarInput.vue";
 
 export default {
   name: 'CarsView',
-  components: {NewCarHeader},
+  components: {NewCarInput, NewCarHeader},
   data: function () {
     return {
       car: {
