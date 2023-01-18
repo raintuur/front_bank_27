9e45dea08b7965576d5384f897b67278b36edb46
Valter Aro <valteraro@gmail.com>
2023-01-18 09:59:25 +0200

    Added a button to the cars website

--- src/views/CarsView.vue
+++ src/views/CarsView.vue
@@ -1,12 +1,24 @@
 <template>
   <div>
+
+    <!--    ROW 1     -->
     <div class="row justify-content-center">
+
+      <!--      COLUMN 1    -->
       <div class="col col-2">
         <input v-model="carMake" type="text" class="form-control" placeholder="Auto mark" aria-label="First name">
       </div>
+
+      <!--      COLUMN 2    -->
       <div class="col col-2">
         <input v-model="carModel" type="text" class="form-control" placeholder="Auto mudel" aria-label="Last name">
       </div>
+
+      <!--      COLUMN 3    -->
+      <div class="col col-2">
+        <button type="button" class="btn btn-outline-success">Success</button>
+      </div>
+
     </div>
   </div>
 </template>
