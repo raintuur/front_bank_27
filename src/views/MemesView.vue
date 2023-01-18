80f279e6c64616d47ffbedd9250ab694eb5ab117
siintrepp <trnsepp@gmail.com>
2023-01-18 10:13:39 +0200

    added memes view and a meme

--- src/views/MemesView.vue
+++ src/views/MemesView.vue
@@ -1,18 +1,26 @@
 <template>
   <div>
+    <!--    ROW 1-->
+    <div>
     <b>Parimad meemid leiad siit!</b>
     <br>
-    <img src="../assets/FB_IMG_1648108888507.jpg" class="img-fluid align-content-md-start"/>
-    <br>
-    <img src="../assets/FB_IMG_1647564529352.jpg" class="img-fluid align-content-md-centerend">
-    <br>
-    <img src="../assets/FB_IMG_1649181808095.jpg" class="img-fluid align-content-md-end"/>
+      <img src="../assets/FB_IMG_1649181808095.jpg" class="img-fluid align-content-md"/>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "MemesView",
+  data: function () {
+    return {
+      memeAnimal: '',
+      memeType: ''
+    }
+  },
+  methods: {
+    addMemeConsept
+  }
 }
 </script>
 
