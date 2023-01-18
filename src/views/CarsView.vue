c06a5a9e6608fac85bfa09d821d1ca5d7108eb39
Mark Saaremägi <mark.saaremagi@gmail.com>
2023-01-18 09:59:20 +0200

    Added button

--- src/views/CarsView.vue
+++ src/views/CarsView.vue
@@ -1,16 +1,24 @@
 <template>
   <div>
 
-
+    <!--  ROW 1 -->
     <div class="row justify-content-center">
 
-      <div class="col col-2">
-        <input v-model="carMake" type="text" class="form-control" placeholder="AUTOMARK" aria-label="First name">
+      <!--COLUMN 1 -->
+      <div class="col col-4">
+        <input v-model="carMake" type="text" class="form-control" placeholder="Auto mark" aria-label="First name">
       </div>
 
-      <div class="col col-2">
-        <input v-model="carModel" type="text" class="form-control" placeholder="AUTOMUDEL" aria-label="Last name">
+      <!--COLUMN 2 -->
+      <div class="col col-4">
+        <input v-model="carModel" type="text" class="form-control" placeholder="Auto mudel" aria-label="Last name">
       </div>
+
+      <!--COLUMN 3-->
+      <div class="col-3">
+        <button type="button" class="btn btn-outline-success">Success</button>
+      </div>
+
 
     </div>
 
@@ -24,8 +32,8 @@ export default {
   name: 'CarsView',
   data: function () {
     return {
-      carMake: 'Audi',
-      carModel: 'A7',
+      carMake: '',
+      carModel: '',
     }
   }
 }
