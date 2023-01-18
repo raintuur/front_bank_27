16879acfd8188b66ab857a6dbe80f4599049da96
Heda <hedakalmus@gmail.com>
2023-01-18 09:59:15 +0200

    button

---
+++ src/views/CarsView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <!-- ROW 1 -->
+
+    <div class="row justify-content-center">
+
+      <!-- COLUMN 1 -->
+      <div class="col col-3">
+        <input v-model="carMake" type="text" class="form-control" placeholder="Auto mark" aria-label="First name">
+      </div>
+
+      <!-- COLUMN 2 -->
+      <div class="col col-3">
+        <input v-model="carModel" type="text" class="form-control" placeholder="Auto mudel" aria-label="Last name">
+      </div>
+
+      <!-- COLUMN 3 -->
+      <div class="col col-3">
+        <button type="button" class="btn btn-outline-success">Success</button>
+      </div>
+
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'CarsView',
+  data: function () {
+    return {
+      carMake: 'Audi',
+      carModel: 'A7'
+    }
+  },
+  methods: {}
+}
+</script>
