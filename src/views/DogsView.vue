79a92bcc5a0ada0c8845282ae9ebb1d75b81afcc
Kivitagapeidus <S88rumail@gmail.com>
2023-01-18 09:17:22 +0200

    Day 2 commit ja push

---
+++ src/views/DogsView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+
+    <div class="row justify-content-center">
+      <div class="col col-2">
+        <input v-model="krantsNimi" class="form-control" placeholder="Krants" aria-label="First name">
+      </div>
+      <div class="col col-2">
+        <input v-model="krantsiPoegNimi" class="form-control" placeholder="Krantsipoeg" aria-label="Last name">
+      </div>
+    </div>
+
+    <br>
+    <img src="https://i.pinimg.com/564x/ef/35/a6/ef35a6daa94bed948fd3d5466457d55e--so-funny-funny-stuff.jpg">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DogsView',
+  data: function () {
+    return {
+      krantsNimi: 'Pontu',
+      krantsiPoegNimi: 'Murdja'
+    }
+  },
+  methods:{
+
+  }
+}
+</script>
