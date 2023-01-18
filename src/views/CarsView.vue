7401a239a2ea0967690742e05fd26df7dad29cee
danieljaatma <daniel.jaatma@gmail.com>
2023-01-18 14:31:57 +0200

    NewCarHeader.vue Commit

--- src/views/CarsView.vue
+++ src/views/CarsView.vue
@@ -4,10 +4,38 @@
 
     <NewCarHeader :company-name="companyName"/>
 
-    <!-- ROW 1 -->
+         <!-- ROW 1 -->
     <NewCarInput :car="car"/>
 
+    <br>
+
+
+    <div class="row">
+
+      <div class="col col-9">
+
+
+
+    </div>
+    <table class="table table-hover table-dark">
+        <thead>
+        <tr>
+          <th>Automark</th>
+          <th>Auto mudel</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="car in cars">
+          <td> {{car.carMake}} </td>
+          <td> {{car.carModel}} </td>
+        </tr>
+        </tbody>
+    </table>
+    </div>
+
+
     <br><br><br>
+
 
     <i><h2>Autopidu</h2>
 
@@ -21,12 +49,15 @@
       Kes küll meile ostab need?
     </i>
 
+
     <br><br><br>
+
 
     <img src="https://img.money.com/2016/08/gettyimages-503375934.jpg"
          alt="" width="500" height="350" style="border:1px solid black" class="my-img">
 
   </div>
+
 
 
 </template>
@@ -42,10 +73,29 @@ export default {
     return {
 
       car: {
+        carId: 0,
         carMake: '',
         carModel: '',
       },
-      cars: [],
+
+      cars: [
+        {
+          carId: 1,
+          carMake: 'Tesla',
+          carModel: 'CyberTruck',
+        },
+    {
+          carId: 2,
+          carMake: 'BMW',
+          carModel: 'X5',
+    },
+    {
+          carId: 3,
+          carMake: 'Ford',
+          carModel: 'Fiesta',
+    }
+    ],
+
       companyName: 'Kurvis Kraavis'
     }
   },
