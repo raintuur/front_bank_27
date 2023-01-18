8d864844e95e910fbc02241f77a029d859c89f3a
Mark Saaremägi <mark.saaremagi@gmail.com>
2023-01-18 15:06:34 +0200

    table added and made it cooler

---
+++ src/components/car/CarsTable.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="row justify-content-center">
+
+    <div class="col col-9">
+
+      <!--autod-->
+      <table class="table table-success table-striped table-hover">
+        <thead>
+        <tr>
+          <th scope="col">Auto mark</th>
+          <th scope="col">Auto mudel</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="car in cars">
+          <td>{{car.carMake}}</td>
+          <td>{{car.carModel}}</td>
+        </tr>
+        </tbody>
+      </table>
+
+    </div>
+
+  </div>
+</template>
+<script>
+export default {
+  name: 'CarsTable',
+  props: {
+    cars: {}
+  }
+}
+</script>
--- src/views/CarsView.vue
+++ src/views/CarsView.vue
@@ -2,63 +2,48 @@
   <div>
     <NewCarHeader :company-name="companyName"/>
     <!--  ROW 1 -->
-    <NewCarInput :car="car"/>
+    <NewCarInput :car="car" @clickAddCarEvent="addCar"/>
+
+    <br>
 
     <!--  ROW 2 -->
-    <div class="row justify-content-center">
-
-      <div class="col col-8">
-
-        <!--autod-->
-        <table class="table table-success table-striped table-hover">
-          <thead>
-          <tr>
-            <th scope="col">Auto mark</th>
-            <th scope="col">Auto mudel</th>
-          </tr>
-          </thead>
-          <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td colspan="2">Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
-          </tbody>
-        </table>
-
-      </div>
-
-    </div>
+    <CarsTable :cars="cars"/>
 
   </div>
 </template>
 
 <script>
-import NewCarHeader from "@/components/NewCarHeader.vue";
+import NewCarHeader from "@/components/car/NewCarHeader.vue";
 import NewCarInput from "@/components/car/NewCarInput.vue";
+import CarsTable from "@/components/car/CarsTable.vue";
 
 export default {
   name: 'CarsView',
-  components: {NewCarInput, NewCarHeader},
+  components: {CarsTable, NewCarInput, NewCarHeader},
   data: function () {
     return {
       car: {
+        carId: 0,
         carMake: '',
         carModel: '',
       },
-      cars: [],
+      cars: [
+        {
+          carId: 1,
+          carMake: 'Audi',
+          carModel: 'R8'
+        },
+        {
+          carId: 2,
+          carMake: 'BMW',
+          carModel: 'X5'
+        },
+        {
+          carId: 3,
+          carMake: 'Tesla',
+          carModel: 'CyberTruck'
+        }
+      ],
       companyName: "Kurvis Kraavis"
     }
   },
@@ -66,6 +51,8 @@ export default {
   methods: {
     addCar: function () {
       alert("Auto " + this.car.carMake + " " + this.car.carModel + " lisati süsteemi")
+     let carAsString = JSON.stringify(this.car)
+      this.cars.push(JSON.parse(carAsString))
     }
   }
 }
