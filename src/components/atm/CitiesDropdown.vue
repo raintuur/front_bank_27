8071273073a7a80eeadf30e0de9093690072dfa3
SILVER <kurriksilver@gmail.com>
2023-01-19 16:06:14 +0200

    SILVER-FEB-2 fixed as per code review

---
+++ src/components/atm/CitiesDropdown.vue
@@ -0,0 +1,46 @@
+<template>
+  <select class="form-select" aria-label="Default select example">
+    <option disabled selected value="-1">vali linn</option>
+    <option value="0">Kõik</option>
+    <option v-for="city in cities" :value="city.cityId">{{ city.cityName }}</option>
+  </select>
+</template>
+<script>
+export default {
+  name: 'CitiesDropdown',
+  data: function () {
+    return {
+      cities: [
+        {
+          cityId: 0,
+          cityName: ''
+        }
+      ]
+    }
+  },
+  methods: {
+    getAllCities: function () {
+      this.$http.get("/all/atm/city")
+          .then(result => {
+            this.cities = result.data
+          })
+          .catch(err => {
+            console.log(err)
+          })
+    },
+  },
+  beforeMount() {
+    this.getAllCities()
+  },
+}
+</script>
+<style scoped>
+select {
+
+}
+
+option {
+  text-align: center;
+  padding-right: 20px;
+}
+</style>
--- src/views/AtmsView.vue
+++ src/views/AtmsView.vue
@@ -1,47 +1,20 @@
 <template>
   <div>
-    <div class="col col-2">
-      <select class="form-select" aria-label="Default select example">
-        <option disabled selected>vali linn</option>
-        <option value="0">Kõik</option>
-        <option v-for="city in cities" :value="city.cityId">{{ city.cityName }}</option>
-      </select>
+    <div class="col col-2 dropdown2">
+      <CitiesDropdown/>
     </div>
   </div>
 </template>
 
 <script>
+import CitiesDropdown from "@/components/atm/CitiesDropdown.vue";
+
 export default {
   name: "AtmsView",
-  data: function () {
-    return {
-      cities: [
-        {
-          cityId: 0,
-          cityName: ''
-        }
-      ]
-    }
-  },
-  methods: {
-    getAllCities: function () {
-      this.$http.get("/all/atm/city")
-          .then(result => {
-            this.cities = result.data
-          })
-          .catch(err => {
-            console.log(err)
-          })
-    },
-  },
-  beforeMount() {
-    this.getAllCities()
-  },
+  components: {CitiesDropdown}
 }
 </script>
 
 <style scoped>
-select {
-  text-align: center;
-}
+
 </style>
