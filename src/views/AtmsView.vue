39311d8d768b248de13fb8ac68e0824279b649a1
indrekjaadla <indrekjaadla@gmail.com>
2023-01-19 16:05:48 +0200

    INDREK-FEB-2 fixed as per codereview.

--- src/views/AtmsView.vue
+++ src/views/AtmsView.vue
@@ -1,50 +1,17 @@
 <template>
   <div>
 
-    <div class="col-2">
-      <select class="form-select" aria-label="Default select example">
-        <option selected disabled value="-1">--Linn--</option>
-        <option value="0">Kõik linnad</option>
-        <option v-for="city in cities" :value="city.cityId">{{city.cityName}}</option>
-      </select>
-    </div>
+    <CitiesDropdown/>
 
   </div>
 </template>
 
 
 <script>
+import CitiesDropdown from "@/components/ATMs/CitiesDropdown.vue";
+
 export default {
   name: "AtmsView",
-  data: function () {
-    return {
-      cities: [
-        {
-          cityId: 0,
-          cityName: ''
-        }
-      ]
-    }
-  },
-  methods: {
-
-    getAllCities: function () {
-
-      this.$http.get("/all/atm/city")
-          .then(result => {
-            this.cities = result.data
-            // vaid siis kui status code ON 200
-
-
-          }).catch(reason => {
-            // vaid siis kui status code EI OLE 200
-      })
-    },
-
-  },
-
-  beforeMount() {
-    this.getAllCities()
-  }
+  components: {CitiesDropdown}
 }
 </script>
