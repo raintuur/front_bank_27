eace4fbb971d9d407ea07814b61ed253b3d8a041
Rain Tüür <rain.tuur@gmail.com>
2023-01-19 16:05:48 +0200

    RAIN-FEB-2 fixed as per code review

---
+++ src/components/atm/CitiesDropdown.vue
@@ -0,0 +1,41 @@
+<template>
+  <select class="form-select" aria-label="Default select example">
+    <option selected disabled value="-1">--Linn--</option>
+    <option value="0">Kõik linnad</option>
+    <option v-for="city in cities" :value="city.cityId">{{ city.cityName }}</option>
+  </select>
+</template>
+<script>
+export default {
+  name: 'CitiesDropdown',
+  data: function () {
+    return {
+      cities: [
+        {
+          cityId: 0,
+          cityName: ''
+        }
+      ]
+    }
+  },
+  methods: {
+
+    getAllCities: function () {
+
+      this.$http.get("/all/atm/city")
+          .then(result => {
+            this.cities = result.data
+
+          }).catch(reason => {
+        // vaid siis kui status code EI OLE 200
+      })
+
+    },
+
+  },
+  beforeMount() {
+    this.getAllCities()
+  }
+
+}
+</script>
