1f5926b464b4c975518caa78cb288222263bfc85
SILVER <kurriksilver@gmail.com>
2023-01-19 15:04:34 +0200

    Added cities dropdown

--- src/views/AtmsView.vue
+++ src/views/AtmsView.vue
@@ -4,7 +4,7 @@
       <select class="form-select" aria-label="Default select example">
         <option disabled selected>vali linn</option>
         <option value="0">Kõik</option>
-        <option v-on:></option>
+        <option v-for="city in cities" :value="city.cityId">{{ city.cityName }}</option>
       </select>
     </div>
   </div>
@@ -13,20 +13,35 @@
 <script>
 export default {
   name: "AtmsView",
+  data: function () {
+    return {
+      cities: [
+        {
+          cityId: 0,
+          cityName: ''
+        }
+      ]
+    }
+  },
   methods: {
     getAllCities: function () {
-      alert("linnade p'ring")
-    }
+      this.$http.get("/all/atm/city")
+          .then(result => {
+            this.cities = result.data
+          })
+          .catch(err => {
+            console.log(err)
+          })
+    },
   },
   beforeMount() {
     this.getAllCities()
-  }
-
+  },
 }
 </script>
 
 <style scoped>
-  select {
-    text-align: center;
-  }
+select {
+  text-align: center;
+}
 </style>
