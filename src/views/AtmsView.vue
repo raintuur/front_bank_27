ddc8eacd0fd8b692332afd99efbe008e31533287
pbernotas <priit.bernotas@gmail.com>
2023-01-19 16:05:42 +0200

    FEB-2 fixed. Dropdown komponendiks.

--- src/views/AtmsView.vue
+++ src/views/AtmsView.vue
@@ -2,11 +2,7 @@
   <div>
 
   <div class="col col-2">
-    <select class="form-select" aria-label="Default select example">
-      <option selected disabled value="-1">--Linn--</option>
-      <option value="0">Koik linnad</option>
-      <option v-for="city in cities" :value="city.cityId">{{city.cityName}}</option>
-    </select>
+    <Citiesdropdown/>
 
   </div>
 
@@ -21,37 +17,11 @@
 </template>
 
 <script>
+import Citiesdropdown from "@/components/atm/Citiesdropdown.vue";
+
 export default {
   name: "AtmsView",
-  data: function () {
-    return {
-    cities: [
-        {
-        cityId: 0,
-        cityName: ''
-        }
-      ]
-    }
-  },
-  methods : {
-    getAllCities: function () {
-
-      this.$http.get("/all/atm/city")
-          .then(result => {
-            this.cities = result.data
-
-          }).catch(reason => {
-
-
-
-      })
-
-    }
-
-  },
-  beforeMount() {
-    this.getAllCities()
-  }
+  components: {Citiesdropdown}
 
 }
 
