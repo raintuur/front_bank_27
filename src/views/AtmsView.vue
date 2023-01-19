8ca86bfb2d54032489ce8d721bc78d450ee10991
Valeria <valeria.metsalluk@gmail.com>
2023-01-19 15:04:36 +0200

    VALERIA-FEB-2 Linnade nimekiri (select)

---
+++ src/views/AtmsView.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+
+    <div class="col-3">
+      <select class="form-select" aria-label="Default select example">
+        <option selected disabled value="-1">--Linn--</option>
+        <option value="0">Kõik linnad</option>
+        <option v-for="city in cities" :value="city.cityId">{{city.cityName}}</option>
+      </select>
+
+    </div>
+
+
+
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "AtmsView",
+  data: function () {
+    return{
+      cities: [
+        {
+          cityId: 0,
+          cityName: ''
+        }
+      ]
+    }
+  },
+  methods: {
+
+    getAllCities: function () {
+      this.$http.get("/all/atm/city")
+          .then(result => {
+            this.cities = result.data
+            // siia satume vaid siis kui STATUS CODE on 200
+          }).catch(reason => {""
+            //siia satume vaid siis kui status code EI OLE 200
+      })
+    }
+  },
+  beforeMount() {
+    this.getAllCities()
+  }
+}
+</script>
