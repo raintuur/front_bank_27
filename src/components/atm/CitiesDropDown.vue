f81a2f3803183f79ae84da631d665a37ec68783e
Kasutaja <roby.palumae@gmail.com>
2023-01-19 16:06:08 +0200

    roby.feb-2 fuzed as per reivew

---
+++ src/components/atm/CitiesDropDown.vue
@@ -0,0 +1,44 @@
+<template>
+  <select class="form-select" aria-label="Default select example">
+    <option selected disabled value="-1">--linn--</option>
+    <option value="0">Kõik linnad</option>
+    <option v-for="city in cities" :value="city.cityId">{{ city.cityName }}</option>
+    -->
+
+  </select>
+</template>
+<script>
+export default {
+  name: 'CitiesDropDown',
+  data: function () {
+
+    return {
+      cities: [
+        {
+          cityId: 0,
+          cityName: ''
+        }
+      ]
+    }
+
+  },
+  methods: {
+
+    getAllCities: function () {
+
+      this.$http.get("/all/atm/city")
+          .then(result => {
+            this.cities = result.data
+            // vaid siis kui status CODE ON 200
+          }).catch(reason => {
+        // vaid siis kui status code EI OLE 200
+      })
+
+    }
+
+  },
+  beforeMount() {
+    this.getAllCities()
+  }
+}
+</script>
--- src/views/AtmsView.vue
+++ src/views/AtmsView.vue
@@ -1,47 +1,18 @@
 <template>
   <div>
     <div class="col-3">
-    <select class="form-select" aria-label="Default select example">
-      <option selected disabled value="-1">--linn--</option>
-      <option value="0">Kõik linnad</option>
-      <option v-for="city in cities" :value="city.cityId">{{city.cityName}}</option>
-
-    </select>
+      <CitiesDropDown/>
     </div>
   </div>
 </template>
 
 <script>
+import CitiesDropDown from "@/components/atm/CitiesDropDown.vue";
+
 export default {
   name: "AtmsView",
-  data: function () {
-
-    return {cities: [
-        {
-          cityId: 0,
-          cityName: ''
-        }
-      ]}
-
-  },
-  methods: {
-
-    getAllCities: function () {
-
-      this.$http.get("/all/atm/city")
-          .then(result => {
-            this.cities = result.data
-            // vaid siis kui status CODE ON 200
-          }).catch(reason => {
-        // vaid siis kui status code EI OLE 200
-      })
-
-    }
-
-  },
-  beforeMount() {
-    this.getAllCities()
-  }
+  components: {CitiesDropDown}
 }
 
 </script>
+
