2679f326450c82884092e2d49d2ad27b9224932d
Juhannes <pstimees@gmail.com>
2023-01-19 15:04:14 +0200

    JUHANNES-FEB-2 Linnade nimekiri (select)

--- src/views/ATMsView.vue
+++ src/views/ATMsView.vue
@@ -4,7 +4,7 @@
       <select class="form-select" aria-label="Default select example">
         <option selected disabled value="-1">--Linn--</option>
         <option value="0">Kõik linnad</option>
-        <option value=?>?</option>
+        <option v-for="city in cities" :value="city.cityId">{{city.cityName}}</option>
       </select>
     </div>
 
@@ -14,13 +14,23 @@
 </template>
 
 <script>
-// import * as http from "http";
 
 export default {
   name: "ATMsView",
+  data: function () {
+    return {
+      cities: [
+        {
+          cityId: 0,
+          cityName: ''
+        }
+      ]
+    }
+  },
   methods: {
     getAllCities: function () {
       this.$http.get("/all/atm/city").then(result => {
+        this.cities = result.data
         //ainult kui code ON 200
       }).catch(reason => {
         //ainult kui code EI OLE 200
