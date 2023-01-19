63ae944fb2e15275d8a0a25d94c320bec98f0962
Kuldar Nei <kuldarnei@gmail.com>
2023-01-19 12:11:17 +0200

    image input seadistamine suvaline img kuva carsView's

---
+++ src/views/AtmsView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div >
+    <div class="col-2">
+
+      <select class="form-select" aria-label="Default select example">
+        <option selected disabled value="-1">--Linn--</option>
+        <option value="0">Kõik linnad</option>
+        <option value="2">Two</option>
+      </select>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AtmsView",
+
+  methods: {
+    getAllCities: function () {
+      alert("linnade päring saadetud")
+    }
+  },
+
+  beforeMount() {
+    this.getAllCities()
+  }
+}
+</script>
