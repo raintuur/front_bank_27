30d1a8efdbc977bee4c92ec1a54fa573a807caaf
Rain Tüür <rain.tuur@gmail.com>
2023-01-19 15:04:08 +0200

    RAIN-FEB-2 Linnade nimekiri (select)

--- src/views/AtmsView.vue
+++ src/views/AtmsView.vue
@@ -5,7 +5,7 @@
       <select class="form-select" aria-label="Default select example">
         <option selected disabled value="-1">--Linn--</option>
         <option value="0">Kõik linnad</option>
-        <option value="?">?</option>
+        <option v-for="city in cities" :value="city.cityId">{{city.cityName}}</option>
       </select>
 
     </div>
@@ -17,22 +17,34 @@
 <script>
 export default {
   name: "AtmsView",
+  data: function () {
+    return {
+      cities: [
+        {
+          cityId: 0,
+          cityName: ''
+        }
+      ]
+    }
+  },
   methods: {
 
     getAllCities: function () {
 
       this.$http.get("/all/atm/city")
           .then(result => {
-            // vaid siis kui status CODE ON 200
+            this.cities = result.data
+
           }).catch(reason => {
-            // vaid siis kui status code EI OLE 200
+        // vaid siis kui status code EI OLE 200
       })
 
-    }
+    },
 
   },
   beforeMount() {
     this.getAllCities()
   }
+
 }
 </script>
