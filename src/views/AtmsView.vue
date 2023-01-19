f0038ebe162871fe651c561fd383793ce0f11088
asyawind <berestovaja@gmail.com>
2023-01-19 12:11:16 +0200

    1

---
+++ src/views/AtmsView.vue
@@ -0,0 +1,31 @@
+<template>
+
+  <div>
+    <div class="dropdown">
+      <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+        Dropdown button
+      </button>
+      <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="#">Action</a></li>
+        <li><a class="dropdown-item" href="#">Another action</a></li>
+        <li><a class="dropdown-item" href="#">Something else here</a></li>
+      </ul>
+    </div>
+  </div>
+
+
+</template>
+
+<script>
+export default {
+  name: "AtmsView"
+  methods: {
+    getAllCities: function () {
+      alert("Linnade paring")
+    }
+  },
+  beforeMount() {
+    this.getAllCities()
+  }
+}
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     devServer: {
-        proxy: 'http://localhost:8080'
+        // proxy: 'http://localhost:8080'
+        proxy: 'https://stoplight.io/mocks/anastasia/myfirstproject/127967583'
     }
 }
