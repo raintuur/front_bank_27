37028a1437dc529ef63fbd807e79cf825d2224c4
Valter Aro <valteraro@gmail.com>
2023-01-19 12:11:24 +0200

    Tegime atmsview

---
+++ src/views/AtmsView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="col col-2">
+      <select class="form-select" aria-label="Default select example">
+        <option selected disabled value="-1">--Linn--</option>
+        <option value="0">kõik linnad</option>
+        <option value="?">?</option>
+      </select>
+    </div>
+
+
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AtmsView",
+  methods: {
+    getAllCities: function () {
+      alert("Linnade päring")
+    }
+  },
+  beforeMount() {
+    this.getAllCities()
+  }
+}
+</script>
