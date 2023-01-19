1be01b80cae14e4bed1ed591cfaa31350f265014
indrekjaadla <indrekjaadla@gmail.com>
2023-01-19 15:04:28 +0200

    INDREK-FEB-2 Linnade nimekiri (select)

--- src/views/AtmsView.vue
+++ src/views/AtmsView.vue
@@ -5,32 +5,44 @@
       <select class="form-select" aria-label="Default select example">
         <option selected disabled value="-1">--Linn--</option>
         <option value="0">Kõik linnad</option>
-        <option value="?">?</option>
+        <option v-for="city in cities" :value="city.cityId">{{city.cityName}}</option>
       </select>
-
     </div>
-
 
   </div>
 </template>
 
+
 <script>
 export default {
   name: "AtmsView",
+  data: function () {
+    return {
+      cities: [
+        {
+          cityId: 0,
+          cityName: ''
+        }
+      ]
+    }
+  },
   methods: {
 
     getAllCities: function () {
 
       this.$http.get("/all/atm/city")
           .then(result => {
-            // vaid siis kui status CODE ON 200
+            this.cities = result.data
+            // vaid siis kui status code ON 200
+
+
           }).catch(reason => {
             // vaid siis kui status code EI OLE 200
       })
-
-    }
+    },
 
   },
+
   beforeMount() {
     this.getAllCities()
   }
