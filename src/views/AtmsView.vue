80419f66f5a84c585e998c923ca964adc2afc546
LadvikM <Ladvik@gmail.com>
2023-01-19 16:06:07 +0200

    MIHKEL-FEB-2 FEB2 FIXED AS REQUESTED

--- src/views/AtmsView.vue
+++ src/views/AtmsView.vue
@@ -2,12 +2,7 @@
   <div>
 
     <div class="col col-3">
-      <select class="form-select" aria-label="Default select example">
-        <option selected disabled value="-1">Linn</option>
-        <option value="0">Kõik linnad</option>
-        <option v-for="city in cities" :value="city.cityId">{{city.cityName}}</option>
-
-      </select>
+      <CitiesDropdown/>
 
     </div>
 
@@ -16,37 +11,13 @@
 </template>
 
 <script>
-import {formToJSON} from "axios";
+
+
+import CitiesDropdown from "@/components/atm/CitiesDropdown.vue";
 
 export default {
   name: "AtmsView",
-  data:function() {
-    return {
-      cities: [
-        {
-          cityId: 0,
-          cityName: ''
-        }
-      ]
-    }
-  },
-  methods: {
-    getAllCities: function () {
-
-      this.$http.get("/all/atm/city")
-          .then(result => {
-            // Kui status code on 200
-            this.cities = result.data
-          }).catch(reason => {
-            // Kui status code ei ole 200
-      })
-
-    }
-
-  },
-  beforeMount() {
-    this.getAllCities()
-  }
+  components: {CitiesDropdown}
 }
 </script>
 
