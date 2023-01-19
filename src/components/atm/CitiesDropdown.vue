88a425dba85151a75a8946a7825469b76e0f6ed0
danieljaatma <daniel.jaatma@gmail.com>
2023-01-19 16:10:22 +0200

    NewCarHeader.vue Commit

---
+++ src/components/atm/CitiesDropdown.vue
@@ -0,0 +1,41 @@
+<template>
+  <select class="form-select" aria-label="Default select example">
+    <option selected disabled value="= "></option>
+
+  </select>
+</template>
+
+<script>
+export default {
+  name: "CitiesDropdown"
+  data: function () {
+    return {
+      cities: [
+        {
+          cityId: 0,
+          cityName: ''
+        }
+      ]
+    }
+  },
+  methods: {
+
+    getAllCities: function () {
+      this.$http.get("/all/atm/city")
+          .then(result => {
+            this.cities = result.data
+
+            // vaid siis kui status CODE ON 200
+          }).catch(reason => {
+        // vaid siis kui status code EI OLE 200
+      })
+
+    },
+
+
+  },
+  beforeMount() {
+    this.getAllCities()
+  }
+}
+</script>
--- src/views/AtmsView.vue
+++ src/views/AtmsView.vue
@@ -2,11 +2,7 @@
   <div>
 
     <div class="col-2">
-      <select class="form-select" aria-label="Default select example">
-        <option selected disabled value="-1">--Linn--</option>
-        <option value="0">Kõik linnad</option>
-        <option value="?">?</option>
-      </select>
+      <CitiesDropdown/>
 
     </div>
 
@@ -16,23 +12,8 @@
 
 <script>
 export default {
+  name: {CitiesDropdown},
   name: "AtmsView",
-  methods: {
 
-    getAllCities: function () {
-
-      this.$http.get("/all/atm/city")
-          .then(result => {
-            // vaid siis kui status CODE ON 200
-          }).catch(reason => {
-            // vaid siis kui status code EI OLE 200
-      })
-
-    }
-
-  },
-  beforeMount() {
-    this.getAllCities()
-  }
 }
 </script>
