4e943cff264fa33796df62980888d11d7de117b2
pbernotas <priit.bernotas@gmail.com>
2023-01-19 12:11:03 +0200

    Added setCarimage function in CarsView.vue.

---
+++ src/views/AtmsView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+
+  <div class="col col-2">
+    <select class="form-select" aria-label="Default select example">
+      <option selected disabled value="-1">--Linn--</option>
+      <option value="0">Koik linnad</option>
+      <option value="2">???</option>
+    </select>
+
+  </div>
+
+
+
+
+
+  </div>
+
+
+
+</template>
+
+<script>
+export default {
+  name: "AtmsView",
+  methods : {
+    getAllCities: function () {
+      alert("Linnade paring")
+    }
+  },
+  beforeMount() {
+    this.getAllCities()
+  }
+
+}
+
+</script>
+
