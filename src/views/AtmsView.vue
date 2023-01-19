e5b1d9633df4dcd5340c5cd5e938ea208b73f14a
SILVER <kurriksilver@gmail.com>
2023-01-19 13:24:19 +0200

    Added table of added cars

---
+++ src/views/AtmsView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="col col-2">
+      <select class="form-select" aria-label="Default select example">
+        <option disabled selected>vali linn</option>
+        <option value="0">Kõik</option>
+        <option v-on:></option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AtmsView",
+  methods: {
+    getAllCities: function () {
+      alert("linnade p'ring")
+    }
+  },
+  beforeMount() {
+    this.getAllCities()
+  }
+
+}
+</script>
+
+<style scoped>
+  select {
+    text-align: center;
+  }
+</style>
