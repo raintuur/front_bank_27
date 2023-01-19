8b80dbffc0dae0ff1727f1fc88ace3b94a2b8a3f
Marii <mtelliskivi@gmail.com>
2023-01-19 16:06:14 +0200

    MARII-FEB-2 Linnade nimekiri (select)

--- src/views/AtmsView.vue
+++ src/views/AtmsView.vue
@@ -2,12 +2,7 @@
   <div>
 
     <div class="col-2">
-      <select class="form-select" aria-label="Default select example">
-        <option selected disabled value="-1">--Linn--</option>
-        <option value="0">Kõik linnad</option>
-        <option v-for="city in cities" :value="city.cityId">{{city.cityName}}</option>
-      </select>
-
+      <CitiesDropdown/>
     </div>
 
 
@@ -15,38 +10,11 @@
 </template>
 
 <script>
+import CitiesDropdown from "@/components/atm/CitiesDropdown.vue";
+
 export default {
   name: "AtmsView",
-  data: function (){
-    return {
-      cities: [
-        {
-          cityId: 0,
-          cityName: ''
-        }
-      ]
-    }
-  },
-  methods: {
+  components: {CitiesDropdown}
 
-    getAllCities: function () {
-
-      this.$http.get("/all/atm/city")
-          .then(result => {
-              this.cities = result.data
-
-            alert(json.stringify(result.data[2].cityName))
-            console.log(Json.stringify)
-            // vaid siis kui status CODE ON 200
-          }).catch(reason => {
-            // vaid siis kui status code EI OLE 200
-      })
-
-    }
-
-  },
-  beforeMount() {
-    this.getAllCities()
-  }
 }
 </script>
