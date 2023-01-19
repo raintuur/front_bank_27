d573766acdc1b5099faffc9b37943266c62b7763
Helar Kärbo <helar.karbo@gmail.com>
2023-01-19 16:05:48 +0200

    HELAR-FEB-2 fixed as per review

---
+++ src/components/atm/CitiesDorpdown.vue
@@ -0,0 +1,36 @@
+<template>
+  <select class="form-select" aria-label="Default select example">
+    <option value="-1" selected disabled>--Linn--</option>
+    <option value="0">Kõik linnad</option>
+    <option v-for="city in cities" :value="city.cityId">{{ city.cityName }}</option>
+  </select>
+</template>
+<script>
+export default {
+  name: 'CitiesDorpdown',
+  data: function () {
+    return {
+      cities: [
+        {
+          cityId: 0,
+          cityName: ''
+        }
+      ]
+    }
+  },
+  methods: {
+    getAllCities: function () {
+      this.$http.get("/all/atm/city")
+          .then(result => {
+            this.cities = result.data
+          }).catch(reason => {
+        // Kui EI OLE 200
+      })
+    }
+
+  },
+  beforeMount() {
+    this.getAllCities()
+  }
+}
+</script>
--- src/views/AtmsView.vue
+++ src/views/AtmsView.vue
@@ -2,42 +2,17 @@
 
   <div class="col-2">
 
-    <select class="form-select" aria-label="Default select example">
-      <option value="-1" selected disabled>--Linn--</option>
-      <option value="0">Kõik linnad</option>
-      <option v-for="city in cities" :value="city.cityId">{{city.cityName}}</option>
-    </select>
+    <CitiesDorpdown/>
 
   </div>
 </template>
 
 <script>
 
+import CitiesDorpdown from "@/components/atm/CitiesDorpdown.vue";
+
 export default {
   name: "AtmsView",
-  data: function () {
-    return {
-      cities: [
-        {
-          cityId: 0,
-          cityName: ''
-        }
-      ]
-    }
-  },
-  methods: {
-    getAllCities: function () {
-      this.$http.get("/all/atm/city")
-          .then(result => {
-            this.cities = result.data
-          }).catch(reason => {
-            // Kui EI OLE 200
-      })
-    }
-
-  },
-  beforeMount() {
-    this.getAllCities()
-  }
+  components: {CitiesDorpdown}
 }
 </script>
