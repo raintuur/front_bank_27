33d93a9ea11e0c5861f75c87d28f67305a2e5828
Marii <mtelliskivi@gmail.com>
2023-01-19 12:11:24 +0200

    commit

---
+++ src/views/AtmsView.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+
+  <div class="col-3"
+  <select class="form-select" aria-label="Default select example">
+    <option selected disabled value="-1">--Linn--</option>
+    <option value="0">Kõik linnad</option>
+    <option value="?">?</option>
+  </select>
+
+</div>
+
+</template>
+
+<script>
+export default {
+  name: "AtmsView"
+  methods: {
+
+    getAllCities: function () {
+      alert("Linnade päring")
+    }
+
+  },
+  beforeMount() {
+    this.getAllCities()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
