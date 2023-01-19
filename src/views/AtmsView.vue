4264052906b32f432e51b31826ee365d3b10ebb6
katrila <katrilattu@gmail.com>
2023-01-19 15:04:09 +0200

    KATRI-FEB-2 Linnade nimekiri (select)

---
+++ src/views/AtmsView.vue
@@ -0,0 +1,53 @@
+<template>
+
+<div class="col-2">
+
+  <select class="form-select" aria-label="Default select example">
+    <option selected disabled>--Linn--</option>
+    <option value="0">Kõik linnad</option>
+    <option v-for="city in cities" :value="city.cityID">{{city.cityName}}</option>
+
+
+  </select>
+
+</div>
+
+</template>
+
+<script>
+export default {
+  name: "AtmsView",
+  data: function () {
+    return{
+      cities: [
+        {
+          cityID: 0,
+          cityName: ''
+        }
+      ]
+    }
+
+  },
+  methods: {
+    getAllCities: function (){
+
+      this.$http.get("/all/atm/city")
+          .then(result => {
+      // vaid siis kui status code on 200
+        this.cities = result.data
+
+          }).catch(reason => {
+        // siia satume vaid siis kui status code EI OLE 200
+      })
+    }
+
+
+  },
+  beforeMount() {
+    this.getAllCities()
+  }
+}
+</script>
+
+
+
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     devServer: {
-        proxy: 'http://localhost:8080'
+        // proxy: 'http://localhost:8080'
+        proxy: 'https://stoplight.io/mocks/k3la2/myproject/127963341'
     }
 }
