aeef9740b879247bf1dcf5d025b407d755400c3b
katrila <katrilattu@gmail.com>
2023-01-19 16:05:53 +0200

    KATRI-FEB-2 fixed as per code review

---
+++ src/components/CitiesDropdown.vue
@@ -0,0 +1,41 @@
+<template>
+  <select class="form-select" aria-label="Default select example">
+    <option selected disabled>--Linn--</option>
+    <option value="0">Kõik linnad</option>
+    <option v-for="city in cities" :value="city.cityID">{{ city.cityName }}</option>
+  </select>
+</template>
+<script>
+export default {
+  name: 'CitiesDropdown',
+  data: function () {
+    return {
+      cities: [
+        {
+          cityID: 0,
+          cityName: ''
+        }
+      ]
+    }
+
+  },
+  methods: {
+    getAllCities: function () {
+
+      this.$http.get("/all/atm/city")
+          .then(result => {
+            // vaid siis kui status code on 200
+            this.cities = result.data
+
+          }).catch(reason => {
+        // siia satume vaid siis kui status code EI OLE 200
+      })
+    }
+
+
+  },
+  beforeMount() {
+    this.getAllCities()
+  }
+}
+</script>
--- src/views/AtmsView.vue
+++ src/views/AtmsView.vue
@@ -2,50 +2,18 @@
 
 <div class="col-2">
 
-  <select class="form-select" aria-label="Default select example">
-    <option selected disabled>--Linn--</option>
-    <option value="0">Kõik linnad</option>
-    <option v-for="city in cities" :value="city.cityID">{{city.cityName}}</option>
-
-
-  </select>
+  <CitiesDropdown/>
 
 </div>
 
 </template>
 
 <script>
+import CitiesDropdown from "@/components/CitiesDropdown.vue";
+
 export default {
   name: "AtmsView",
-  data: function () {
-    return{
-      cities: [
-        {
-          cityID: 0,
-          cityName: ''
-        }
-      ]
-    }
-
-  },
-  methods: {
-    getAllCities: function (){
-
-      this.$http.get("/all/atm/city")
-          .then(result => {
-      // vaid siis kui status code on 200
-        this.cities = result.data
-
-          }).catch(reason => {
-        // siia satume vaid siis kui status code EI OLE 200
-      })
-    }
-
-
-  },
-  beforeMount() {
-    this.getAllCities()
-  }
+  components: {CitiesDropdown}
 }
 </script>
 
