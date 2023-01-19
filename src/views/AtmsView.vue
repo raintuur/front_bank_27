7baab7088163cd8f8e0a43021a9e3ceb06f121fe
danieljaatma <daniel.jaatma@gmail.com>
2023-01-19 12:39:11 +0200

    NewCarHeader.vue Commit

---
+++ src/views/AtmsView.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+
+  <div class = "col-2">
+  <select class="form-select" aria-label="Default select example">
+    <option selected disabled="-1">--Linn--</option>
+    <option value="0">Kõik linnad</option>
+    <option value="?">?</option>
+  </select>
+
+  </div>
+
+</div>
+
+</template>
+
+
+<script>
+export default {
+  name: "AtmsView",
+  methods: {
+    getAllCities: function (){
+      alert("Linnade päring")
+    }
+  },
+  beforeMount() {
+    this.getAllCities()
+  }
+}
+</script>
