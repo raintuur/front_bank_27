d01655bbe581ea8b370f7e9e26e793517ea9dbb2
Kivitagapeidus <S88rumail@gmail.com>
2023-01-19 15:04:29 +0200

    väljad töötavad paremini

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/dogs">Dogs</router-link> |
+      <router-link to="/atms">ATMs</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
     <router-view/>
---
+++ src/views/AtmsView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="row justify-content-center">
+    <div class="col-2">
+      <select class="form-select" aria-label="Default select example">
+        <option selected value="0" disabled>--Linn--</option>
+        <option value="0">Kõik linnad</option>
+        <option v-for="city in cities" :value="city.cityId">{{ city.cityName }}</option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AtmsView",
+  data: function (){
+    return{
+      cities: [
+        {
+          cityId: 0,
+          cityName: '',
+        }
+      ]
+    }
+  },
+  methods: {
+    getAllCities: function () {
+      this.$http.get("/all/atm/city")
+          .then(result => {
+            this.cities = result.data
+          }).catch(reason => alert("ERRORRRRRRRRRRRRRR"))
+    }
+  },
+  beforeMount() {
+    this.getAllCities()
+  }
+}
+</script>
