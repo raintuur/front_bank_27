613848105a700b63b9a51c0e50ef8569764ecb71
Mark Saaremägi <mark.saaremagi@gmail.com>
2023-01-19 12:11:26 +0200

    tabel ja before mount ja proxy ja veel midagi

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
     devServer: {
-        proxy: 'http://localhost:8080'
+        //proxy: 'http://localhost:8080'
+        proxy: 'https://stoplight.io/mocks/mark321/myproject/127967650'
+
     }
 }
