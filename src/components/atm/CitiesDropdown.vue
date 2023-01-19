f32591ec41a3a8de1ac63c4f5d073487d209af88
Kivitagapeidus <S88rumail@gmail.com>
2023-01-19 16:06:07 +0200

    SÖREN FEB-2 Fixed

---
+++ src/components/atm/CitiesDropdown.vue
@@ -0,0 +1,33 @@
+<template>
+  <select class="form-select" aria-label="Default select example">
+    <option selected value="0" disabled>--Linn--</option>
+    <option value="0">Kõik linnad</option>
+    <option v-for="city in cities" :value="city.cityId">{{ city.cityName }}</option>
+  </select>
+</template>
+<script>
+export default {
+  name: 'CitiesDropdown',
+  data: function (){
+    return{
+      cities: [
+        {
+          cityId: 0,
+          cityName: '',
+        }
+      ]
+    }
+  },
+  methods: {
+    getAllCities: function () {
+      this.$http.get("/all/atm/city")
+          .then(result => {
+            this.cities = result.data
+          }).catch(reason => alert("ERRORRRRRRRRRRRRRR"))
+    }
+  },
+  beforeMount() {
+    this.getAllCities()
+  }
+}
+</script>
--- src/views/AtmsView.vue
+++ src/views/AtmsView.vue
@@ -1,38 +1,16 @@
 <template>
   <div class="row justify-content-center">
     <div class="col-2">
-      <select class="form-select" aria-label="Default select example">
-        <option selected value="0" disabled>--Linn--</option>
-        <option value="0">Kõik linnad</option>
-        <option v-for="city in cities" :value="city.cityId">{{ city.cityName }}</option>
-      </select>
+      <CitiesDropdown/>
     </div>
   </div>
 </template>
 
 <script>
+import CitiesDropdown from "@/components/atm/CitiesDropdown.vue";
+
 export default {
   name: "AtmsView",
-  data: function (){
-    return{
-      cities: [
-        {
-          cityId: 0,
-          cityName: '',
-        }
-      ]
-    }
-  },
-  methods: {
-    getAllCities: function () {
-      this.$http.get("/all/atm/city")
-          .then(result => {
-            this.cities = result.data
-          }).catch(reason => alert("ERRORRRRRRRRRRRRRR"))
-    }
-  },
-  beforeMount() {
-    this.getAllCities()
-  }
+  components: {CitiesDropdown},
 }
 </script>
