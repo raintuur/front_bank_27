3ddccdb7dfd2053ab7b6329b7cec15c07fccaeb4
Janeli <janelikurm@gmail.com>
2023-01-19 12:11:28 +0200

    Last version

---
+++ src/views/AtmsView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+
+    <div class="col-2">
+<select class="form-select" aria-label="Default select example">
+      <option selected disabled>--Linn--</option>
+      <option value="1">Kõik linnad</option>
+      <option value="2">Tallinn</option>
+      <option value="3">Tartu</option>
+      <option value="3">Pärnu</option>
+    </select>
+  </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AtmsView",
+  methods: {
+    getAllCities: function () {
+      alert("Linnade päring")
+    }
+  },
+  beforeMount() {
+    this.getAllCities()
+  }
+}
+</script>
