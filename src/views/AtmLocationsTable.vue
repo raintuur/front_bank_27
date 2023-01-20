c5bdb17158bc3f99057b2b25c7bd2e6a54e7ec55
danieljaatma <daniel.jaatma@gmail.com>
2023-01-20 10:37:19 +0200

    NewCarHeader.vue Commit

---
+++ src/views/AtmLocationsTable.vue
@@ -0,0 +1,25 @@
+<template>
+  <table class="table table-hover table-dark">
+    <thead>
+    <tr>
+      <th scope="col">Linn</th>
+      <th scope="col">Asukoht</th>
+      <th scope="col">Teenused</th>
+    </tr>
+    </thead>
+    <tbody>
+
+    <!--  Allolevat elementi soovime JSON massiivi for loopiga genereerida      -->
+    <tr>
+      <td>?</td>
+      <td>?</td>
+      <td>?</td>
+    </tr>
+    </tbody>
+  </table>
+</template>
+<script>
+export default {
+  name: 'AtmLocationsTable',
+}
+</script>
--- src/views/AtmsView.vue
+++ src/views/AtmsView.vue
@@ -1,20 +1,97 @@
 <template>
   <div>
+    <div class="row justify-content-center">
 
     <div class="col-2">
       <CitiesDropdown/>
     </div>
 
 
+
+
+
+    <div class="col-5">
+      <AtmLocationsTable/>
+    </div>
+
+  </div>
   </div>
 </template>
 
+
+
 <script>
 import CitiesDropdown from "@/components/atm/CitiesDropdown.vue";
+import AtmLocationsTable from "@/views/AtmLocationsTable.vue";
 
 export default {
   name: "AtmsView",
-  components: {CitiesDropdown}
+  components: {AtmLocationsTable, CitiesDropdown},
+  data: function () {
+    return{
+      atmLocations: [
+        [
+          {
+             locationId : 0,
+             locationName :'',
+             cityName :   ,
+             transactionTypes : [
+              {
+                 typeName :   
+              },
+              {
+                 typeName :   
+              },
+              {
+                 typeName :   
+              }
+            ]
+          },
+          {
+             locationId : 2,
+             locationName :   ,
+             cityName :   ,
+             transactionTypes : [
+              {
+                 typeName :   
+              },
+              {
+                 typeName :
+              }
+            ]
+          },
+          {
+             locationId : 3,
+             locationName :  ,
+             cityName : ,
+             transactionTypes : [
+              {
+                 typeName :
+              },
+              {
+                 typeName :   
+              }
+            ]
+          }
+        ]
+          
+      ]
+    }
+  },
 
+  methods: {
+    getAllAtmLocations: function () {
+      this.$http.get(url: "all/atm/locations")
+          .then(response =>) {
+        // soovime andmeid kusagile muutujasse panna
+      }
+        this.atmLocations = response.data
+
+        console.log(response.data)
+      })
+      .catch(error =>) {
+      console.log(error)
+    })
+  },
 }
 </script>
