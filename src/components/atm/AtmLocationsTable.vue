1714d4450606a71829ba8b3d29132cde64c2ee04
Rain Tüür <rain.tuur@gmail.com>
2023-01-20 11:37:00 +0200

    RAIN-FEB-2 fixed as per code review

--- src/components/atm/AtmLocationsTable.vue
+++ src/components/atm/AtmLocationsTable.vue
@@ -10,10 +10,14 @@
     <tbody>
 
     <!--  Allolevat elemnti soovime JSON massivi for loopiga genereerida  -->
-    <tr>
-      <td>?</td>
-      <td>?</td>
-      <td>?</td>
+    <tr v-for="atmLocation in atmLocations">
+      <td>{{atmLocation.cityName}}</td>
+      <td>{{atmLocation.locationName}}</td>
+      <td>
+        <div v-for="transactionType in atmLocation.transactionTypes">
+          {{transactionType.typeName}}
+        </div>
+      </td>
     </tr>
     </tbody>
   </table>
