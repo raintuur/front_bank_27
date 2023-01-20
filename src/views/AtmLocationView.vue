eea527e3c865ffe4080672f460d375f5ba120968
Rain Tüür <rain.tuur@gmail.com>
2023-01-20 16:22:01 +0200

    RAIN-FEB-3 WIP

--- src/views/AtmLocationView.vue
+++ src/views/AtmLocationView.vue
@@ -6,15 +6,9 @@
   <div class="row justify-content-center">
     <div class="col-2">
 
-      <div class="input-group mb-3">
-        <div class="input-group-text">
-          <input class="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input">
-        </div>
-        <input type="text" disabled class="form-control" aria-label="Text input with checkbox">
-      </div>
+      <TransactionTypeCheckBox/>
 
     </div>
-
   </div>
 
 
@@ -25,7 +19,24 @@
 </template>
 
 <script>
+import TransactionTypeCheckBox from "@/views/TransactionTypeCheckBox.vue";
+
 export default {
-  name: "AtmLocationView"
+  name: "AtmLocationView",
+  components: {TransactionTypeCheckBox},
+  data: function () {
+    return {
+      transactionTypes: [
+        {
+          typeId: 0,
+          typeName: '',
+          isSelected: false
+        }
+      ]
+    }
+  },
+  methods: {
+
+  }
 }
 </script>
---
+++ src/views/TransactionTypeCheckBox.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="form-check">
+    <input class="form-check-input" type="checkbox">
+    <label class="form-check-label">
+      ?
+    </label>
+  </div>
+</template>
+<script>
+export default {
+  name: 'TransactionTypeCheckBox'
+}
+</script>
