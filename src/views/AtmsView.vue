79da4ccfe0d13fb6e8934e24dfcae7b1ce0cd99e
Marii <mtelliskivi@gmail.com>
2023-01-20 10:37:09 +0200

    commit

--- src/views/AtmsView.vue
+++ src/views/AtmsView.vue
@@ -1,9 +1,50 @@
 <template>
   <div>
 
+    <div class="row justify-content-center">
+
     <div class="col-2">
       <CitiesDropdown/>
     </div>
+
+<!--  TABEL    -->
+    <div class="col-5">
+
+      <table class="table table-hover table-dark">
+        <thead>
+        <tr>
+          <th scope="col">Linn</th>
+          <th scope="col">Asukoht</th>
+          <th scope="col">Teenused</th>
+        </tr>
+        </thead>
+        <tbody>
+<!--    Allolevat elementi soovime JSON massiivi for loopiga genereerida    -->
+        <tr>
+          <td>?</td>
+          <td>?</td>
+          <td>@?</td>
+        </tr>
+        </tbody>
+
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td colspan="2">Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+        </tbody>
+      </table>
+
+      <div
+
+
+
+
 
 
   </div>
