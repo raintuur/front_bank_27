58167e76d56c12ec399c679f5012b980c2304812
pbernotas <priit.bernotas@gmail.com>
2023-01-20 10:43:09 +0200

    FEB-2 fixed. Dropdown komponendiks.

--- src/views/AtmsView.vue
+++ src/views/AtmsView.vue
@@ -1,27 +1,32 @@
 <template>
   <div>
+    <div class="row justify-content-center">
 
-  <div class="col col-2">
-    <Citiesdropdown/>
 
+      <div class="col-2">
+        <Citiesdropdown/>
+      </div>
+
+      <div class="col-5">
+
+        <AtmLocationsTable/>
+
+      </div>
+
+    </div>
   </div>
-
-
-
-
-
-  </div>
-
 
 
 </template>
 
 <script>
 import Citiesdropdown from "@/components/atm/Citiesdropdown.vue";
+import AtmLocationsTable from "@/components/atm/AtmLocationsTable.vue";
 
 export default {
   name: "AtmsView",
-  components: {Citiesdropdown}
+  components: {AtmLocationsTable, Citiesdropdown}
+
 
 }
 
