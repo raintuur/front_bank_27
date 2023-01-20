f350bf573b7cdeb24cd02658e44addcadab51d01
Valter Aro <valteraro@gmail.com>
2023-01-20 16:24:35 +0200

    VALTER-FEB-3 added getAllAtmLocations function

--- src/views/TransactionTypeCheckBox.vue
+++ src/views/TransactionTypeCheckBox.vue
@@ -1,13 +1,29 @@
 <template>
-  <div class="form-check">
-    <input class="form-check-input" type="checkbox">
-    <label class="form-check-label">
-      ?
-    </label>
+  <div>
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox">
+      <label class="form-check-label">
+        ?
+      </label>
+    </div>
   </div>
+
 </template>
 <script>
 export default {
   name: 'TransactionTypeCheckBox'
+}, data: function () {
+  return {
+    transactionTypes: [
+      {
+        typeId: 0,
+        typeName: '',
+        isSelected: false
+      }
+    ]
+  }
+},
+methods: {
+
 }
 </script>
