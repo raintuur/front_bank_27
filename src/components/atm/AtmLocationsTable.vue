60258b938ecbc8567875f0dd4156d459dd825d95
Janeli <janelikurm@gmail.com>
2023-01-20 11:01:09 +0200

    JANELI-FEB-2 fixed as per comment

---
+++ src/components/atm/AtmLocationsTable.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="col-4">
+    <table class="table">
+      <thead>
+      <tr>
+        <th scope="col">Linn</th>
+        <th scope="col">Asukoht</th>
+        <th scope="col">Teenused</th>
+      </tr>
+      </thead>
+      <tbody>
+
+      <!--Allolevame elementi soovime JSOn massiivi for loopiga genereerida-->
+      <tr>
+        <td>?</td>
+        <td>?</td>
+        <td>?</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  name: 'AtmLocationsTable',
+  data: function () {
+    return {
+      atmLocations: [
+        {
+          locationId: 0,
+          locationName: '',
+          cityName: '',
+          transactionTypes: [
+            {
+              typeName: ''
+            }
+          ]
+        }
+      ]
+    }
+  },
+  methods: {
+    getAllAtmLocations: function () {
+      this.$http.get("/all/atm/locations")
+          .then(response => {
+            // soovime andmeid kuhugi muutujasse panna
+            this.atmLocations = response.data
+          })
+          .catch(error => {
+            console.log(error)
+          })
+    },
+  },
+  beforeMount() {
+    this.getAllAtmLocations()
+  }
+}
+</script>
--- src/components/atm/AtmsView.vue
+++ src/components/atm/AtmsView.vue
@@ -1,10 +1,17 @@
 <template>
   <div>
+    <!--  ROW 1 -->
+    <div class="row justify-content-center">
 
-    <div class="col-2">
-      <CitiesDropdown/>
+
+      <div class="col-2">
+        <CitiesDropdown/>
+      </div>
+
+
+      <!-- TABEL -->
+      <AtmLocationsTable/>
     </div>
-
   </div>
 </template>
 
@@ -12,9 +19,14 @@
 
 
 import CitiesDropdown from "@/views/CitiesDropdown.vue";
+import AtmLocationsTable from "@/components/atm/AtmLocationsTable.vue";
 
 export default {
   name: "AtmsView",
-  components: {CitiesDropdown}
+  components: {AtmLocationsTable, CitiesDropdown},
+
 }
+
+
+
 </script>
