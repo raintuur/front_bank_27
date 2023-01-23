605e4f020ad22d616836e3827d705236fd99b3dc
Valter Aro <valteraro@gmail.com>
2023-01-23 16:41:40 +0200

    VALTER-FEB-4 added stuff

--- src/views/AtmLocationView.vue
+++ src/views/AtmLocationView.vue
@@ -5,31 +5,32 @@
     <div class="row justify-content-center">
 
       <div class="col-2">
-        <CitiesDropdown/>
+        <CitiesDropdown @citiesDropdownOnChangeEvent="setCityId"/>
       </div>
 
       <!--  COLUMN 2  -->
       <!--  LOCATION NAME  -->
-      <div class="col-2">
+      <div class="col-3">
 
         <div class="input-group mb-3">
-          <span class="input-group-text">Asukoht</span>
-          <input v-model="locationName" type="text" class="form-control">
+          <span class="input-group-text " :class="{'input-success' :atmRequest.locationName !== ''}">Asukoht</span>
+          <input v-model="atmRequest.locationName" type="text" class="form-control">
         </div>
 
         <div class="input-group mb-3">
-          <span class="input-group-text">Automaatide arv</span>
-          <input v-model="numberOfAtms" type="number" min="10" class="form-control">
+          <span class="input-group-text" :class="{'input-success' :Number(atmRequest.numberOfAtms) > 0}">Automaatide arv</span>
+          <input v-model="atmRequest.numberOfAtms"  type="number" min="0" class="form-control">
         </div>
 
 
         <TransactionTypeCheckBox/>
-
-
+        <ImageInput @pictureInputSuccess="setPictureBase64Data"/>
+          <button v-on:click="addAtmLocation" type="button" class="btn btn-success">Salvesta</button>
       </div>
 
       <!--  COLUMN 3  -->
-      <div class="col-2">
+      <div class="col-3">
+        <img :src="pictureData" class="img-thumbnail">
 
 
       </div>
@@ -45,29 +46,48 @@ import CitiesDropdown from "@/components/atm/CitiesDropdown.vue";
 import LocationName from "@/components/atm/new/location_name/LocationName.vue";
 import NumberOfAtms from "@/components/atm/new/number_of/NumberOfAtms.vue";
 import AlertDanger from "@/components/alert/AlertDanger.vue";
+import ImageInput from "@/components/ImageInput.vue";
 
 export default {
   name: "AtmLocationView",
-  components: {AlertDanger, NumberOfAtms, LocationName, CitiesDropdown, TransactionTypeCheckBox},
+  components: {ImageInput, AlertDanger, NumberOfAtms, LocationName, CitiesDropdown, TransactionTypeCheckBox},
   data: function () {
     return {
-      locationName: '',
-      numberOfAtms: 0,
+      atmRequest: {
+        cityId: 0,
+        locationName: '',
+        numberOfAtms: 0,
+        picture: '',
+        transactionTypes: [
+          {
+            typeId: 0,
+            typeName: '',
+            isSelected: true,
+          }
+        ]
+      }
+
 
     }
   },
   methods: {
-
-    someMethodName: function () {
-
-      this.$http.get("/some/path")
-          .then(response => {
-            console.log(response.data)
-          })
-          .catch(error => {
-            console.log(error)
-          })
+    setPictureBase64Data: function (pictureBase64Data) {
+      this.atmRequest.picture = pictureBase64Data
     },
+    addAtmLocation: function () {
+      this.atmRequest.numberOfAtms = Number()
+      this.$http.post("/atm/location", this.atmRequest
+      ).then(response => {
+        console.log(response.data)
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+
+    setCityId: function (cityId) {
+      this.atmRequest.cityId = cityId
+    }
+
   }
 }
 </script>
