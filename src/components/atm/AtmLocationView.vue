799043e893cb259af74b7eca9b1daf5c0aa00a2d
asyawind <berestovaja@gmail.com>
2023-01-23 10:45:55 +0200

    feb2 fixed

---
+++ src/components/atm/AtmLocationView.vue
@@ -0,0 +1,50 @@
+<template>
+<div>
+
+  <!--  ROW 1  -->
+  <div class="row justify-content-center">
+    <div class="col-2">
+      <CitiesDropdown/>
+    </div>
+<div class="col-2">
+  <AtmLocationNameInput :location-name="locationName"/>
+
+  <div class="input-group mb-3">
+    <span class="input-group-text">Automaatide arv</span>
+    <input type="number" class="form-control">
+  </div>
+
+<div>
+  <TransactionTypeCheckBox/>
+</div>
+  </div>
+
+
+    </div>
+  </div>
+
+
+
+
+
+</div>
+</template>
+
+<script>
+import TransactionTypeCheckBox from "@/views/TransactionTypeCheckBox.vue";
+import CitiesDropdown from "@/components/atm/CitiesDropdown.vue";
+import AtmLocationNameInput from "@/components/atm/AtmLocationNameInput.vue";
+
+export default {
+  name: "AtmLocationView",
+  components: {AtmLocationNameInput, CitiesDropdown, TransactionTypeCheckBox},
+  data: function () {
+    return {
+locationName: ''
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
