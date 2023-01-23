324b953f8bed819eeb51bf63a1a37e31bc84481a
indrekjaadla <indrekjaadla@gmail.com>
2023-01-23 13:49:44 +0200

    Task complete.

---
+++ src/views/CitiesButtons.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="col-2 d-grid gap-2">
+    <button type="button" class="btn btn-outline-success">Kõik</button>
+    <button v-for="city in cities" class="btn btn-outline-danger">{{city.cityName}}</button>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'CitiesButtons',
+  data: function () {
+    return {
+      cities: [
+        {
+          cityId: 0,
+          cityName: ''
+        }
+      ]
+    }
+  },
+  methods: {
+    getAllCities: function () {
+      this.$http.get("/library/cities")
+          .then(result => {
+            this.cities = result.data
+          })
+          .catch(error => {
+            console.log(error)
+          })
+    }
+  },
+  beforeMount() {
+    this.getAllCities()
+  }
+}
+</script>
---
+++ src/views/LibrariesView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <CitiesButtons/>
+  </div>
+</template>
+
+
+<script>
+import CitiesButtons from "@/views/CitiesButtons.vue";
+
+export default {
+  name: "LibrariesView",
+  components: {CitiesButtons}
+}
+</script>
