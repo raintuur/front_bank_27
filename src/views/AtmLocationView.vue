d62227982761a4ff33a4ea67d99701c3e3a5d2e0
Mark Saaremägi <mark.saaremagi@gmail.com>
2023-01-23 10:10:21 +0200

    MARK-FEB-4 added input

--- src/views/AtmLocationView.vue
+++ src/views/AtmLocationView.vue
@@ -2,10 +2,29 @@
   <div>
 
   <div class="row justify-content-center">
+<!--cloumn1-->
     <div class="col-2">
-
+      <CitiesDropdown/>
+    </div>
+<!--column2-->
+    <div class="col-2">
+      <div class="input-group mb-3">
+        <div class="input-group-prepend">
+          <span class="input-group-text" id="basic-addon3">Asukoht</span>
+        </div>
+        <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+      </div>
+      <div class="input-group mb-3">
+        <div class="input-group-prepend">
+          <span class="input-group-text" id="basic-addon3">Automaatide arv</span>
+        </div>
+        <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+      </div>
       <TransactionTypeCheckBox/>
+    </div>
 
+<!--    column 3 -->
+    <div class="col-2">
 
     </div>
   </div>
@@ -17,10 +36,11 @@
 
 <script>
 import TransactionTypeCheckBox from "@/views/TransactionTypeCheckBox.vue";
+import CitiesDropdown from "@/components/atm/CitiesDropdown.vue";
 
 export default {
   name: "AtmLocationView",
-  components: {TransactionTypeCheckBox},
+  components: {CitiesDropdown, TransactionTypeCheckBox},
   data: function () {
     return {
     }
