2776271e15afdbd888b2f9d69bb1cefd14c67e7a
Janeli <janelikurm@gmail.com>
2023-01-23 12:57:04 +0200

    new beginnings

---
+++ src/components/library/CitiesButtons.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="col-2">
+
+    <div class="row">
+      <button type="button" class="btn btn-outline-secondary">Kõik linnad</button>
+    </div>
+    <div class="row">
+      <button v-for="city in cities" type="button" class="btn btn-outline-secondary">{{city.cityName}}</button>
+    </div>
+    <div class="row">
+      <button type="button" class="btn btn-outline-secondary"> ??</button>
+    </div>
+    <div class="row">
+      <button type="button" class="btn btn-outline-secondary"> ??</button>
+    </div>
+
+  </div>
+</template>
+<script>
+export default {
+  name: 'CitiesButtons',
+  data: function () {
+    return {
+      cities: [{
+        cityId: 0,
+        cityName: '',
+      }
+      ]
+    }
+  },
+  methods: {
+    getCitiesName: function () {
+      this.$http.get("/library/cities")
+          .then(response => {
+            this.cities(response.data)
+          })
+          .catch(error => {
+            console.log(error)
+          })
+    },
+  },
+  beforeMount() {
+    this.getCitiesName()
+  }
+}
+
+</script>
