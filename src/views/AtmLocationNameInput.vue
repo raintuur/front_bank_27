4bfaecfe833185996413a4238ea20ae249534d95
Marii <mtelliskivi@gmail.com>
2023-01-23 10:59:41 +0200

    commit

---
+++ src/views/AtmLocationNameInput.vue
@@ -0,0 +1,32 @@
+<template>
+
+  <div v-if="locationName == ''">
+
+  <div class="input-group mb-3">
+    <span class="input-group-text">Asukoht</span>
+    <input v-model="numberOfAtms" type="text" class="form-control">
+  </div>
+
+
+  </div>
+  <div v-else>
+
+    <div class="input-group mb-3">
+      <span class="input-group-text input">Asukoht</span>
+      <input v-model="locationName" type="text" class="form-control">
+    </div>
+
+  </div>
+
+
+
+</template>
+<script>
+export default {
+  name: 'AtmLocationNameInput',
+  props: {
+    locationName: {},
+    numberOfAtms: 0,
+  }
+}
+</script>
--- src/views/AtmLocationView.vue
+++ src/views/AtmLocationView.vue
@@ -4,9 +4,27 @@
 
   <!--  ROW 1  -->
   <div class="row justify-content-center">
+
     <div class="col-2">
+         <CitiesDropdown/>
+    </div>
+
+    <!--      COLUMN 2    -->
+    <div class="col-2">
+      <AtmLocationNameInput :location-name="locationName"/>
+
+    <div class="input-group mb-3">
+      <span class="input-group-text" >Automaatide arv</span>
+      <input type="number" class="form-control">
+    </div>
 
       <TransactionTypeCheckBox/>
+    </div>
+
+
+
+    <div class="col-2">
+
 
     </div>
   </div>
@@ -20,12 +38,15 @@
 
 <script>
 import TransactionTypeCheckBox from "@/views/TransactionTypeCheckBox.vue";
+import CitiesDropdown from "@/components/atm/CitiesDropdown.vue";
+import AtmLocationNameInput from "@/views/AtmLocationNameInput.vue";
 
 export default {
   name: "AtmLocationView",
-  components: {TransactionTypeCheckBox},
+  components: {AtmLocationNameInput, CitiesDropdown, TransactionTypeCheckBox},
   data: function () {
     return {
+      locationName: {}
 
     }
   },
