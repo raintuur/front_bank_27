f72e474301d9443084b059838a19856c2858ccac
katrila <katrilattu@gmail.com>
2023-01-23 13:49:34 +0200

    KATRI-FL-2 complete

---
+++ src/views/LibrariesView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+
+
+    <LibraryLocations/>
+  </div>
+</template>
+
+<script>
+import LibraryLocations from "@/components/library/LibraryLocations.vue";
+
+
+export default {
+  name: "LibrariesView",
+  components: {LibraryLocations}
+}
+</script>
+
