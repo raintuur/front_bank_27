e1508ce43aa2999f0a7ef3e1279cfb2d389ac693
Kivitagapeidus <S88rumail@gmail.com>
2023-01-23 11:00:10 +0200

    SÖREN

--- src/views/AtmLocationView.vue
+++ src/views/AtmLocationView.vue
@@ -2,7 +2,16 @@
   <div>
     <div class="row justify-content-center">
       <div class="col-2">
+        <CitiesDropdown/>
+      </div>
+      <div class="col-2">
+        <AtmLocationNameInput :location-name="locationName"/>
+        <NumberOfAtmsInput :number-of-atms="numberOfAtms"/>
         <TransactionTypeCheckBox/>
+
+      </div>
+      <div class="col-2">
+
       </div>
     </div>
   </div>
@@ -10,14 +19,18 @@
 </template>
 
 <script>
-import TransactionTypeCheckBox from "@/components/TransactionTypeCheckBox.vue";
+import TransactionTypeCheckBox from "@/components/atm/TransactionTypeCheckBox.vue";
+import CitiesDropdown from "@/components/atm/CitiesDropdown.vue";
+import AtmLocationNameInput from "@/components/atm/AtmLocationNameInput.vue";
+import NumberOfAtmsInput from "@/views/NumberOfAtmsInput.vue";
 
 export default {
   name: "AtmLocationView",
-  components: {TransactionTypeCheckBox},
+  components: {NumberOfAtmsInput, AtmLocationNameInput, CitiesDropdown, TransactionTypeCheckBox},
   data: function (){
     return {
-
+      locationName: '',
+      numberOfAtms: 0
 
     }
   },
---
+++ src/views/NumberOfAtmsInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+  <div v-if="Number(numberOfAtms) === 0">
+    <div class="input-group mb-3">
+      <span class="input-group-text">Automaatide arv</span>
+      <input v-model="numberOfAtms" type="number" class="form-control">
+    </div>
+  </div>
+  <div v-else>
+    <div class="input-group mb-3">
+      <span class="input-group-text input-filled">Automaatide arv</span>
+      <input v-model="numberOfAtms" type="number" class="form-control">
+    </div>
+  </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'NumberOfAtmsInput',
+  props: {
+    numberOfAtms: {}
+  }
+}
+</script>
