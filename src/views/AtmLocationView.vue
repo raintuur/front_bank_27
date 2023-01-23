223598cd0c78f4b36846ee2fc9a396902fcebfb8
Rain Tüür <rain.tuur@gmail.com>
2023-01-23 10:09:40 +0200

    RAIN-FEB-4 WIP

--- src/views/AtmLocationView.vue
+++ src/views/AtmLocationView.vue
@@ -1,36 +1,50 @@
 <template>
-<div>
+  <div>
+
+    <!--  ROW 1  -->
+    <div class="row justify-content-center">
+
+      <div class="col-2">
+        <CitiesDropdown/>
+      </div>
+
+      <!--  COLUMN 2  -->
+      <div class="col-2">
+        <div class="input-group mb-3">
+          <span class="input-group-text" id="basic-addon3">Asukoht</span>
+          <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+        </div>
+
+        <div class="input-group mb-3">
+          <span class="input-group-text" id="basic-addon3">Automaatide arv</span>
+          <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+        </div>
+        <TransactionTypeCheckBox/>
 
 
-  <!--  ROW 1  -->
-  <div class="row justify-content-center">
-    <div class="col-2">
+      </div>
 
-      <TransactionTypeCheckBox/>
+      <!--  COLUMN 3  -->
+      <div class="col-2">
 
+
+      </div>
     </div>
+
+
   </div>
-
-
-
-
-
-</div>
 </template>
 
 <script>
 import TransactionTypeCheckBox from "@/views/TransactionTypeCheckBox.vue";
+import CitiesDropdown from "@/components/atm/CitiesDropdown.vue";
 
 export default {
   name: "AtmLocationView",
-  components: {TransactionTypeCheckBox},
+  components: {CitiesDropdown, TransactionTypeCheckBox},
   data: function () {
-    return {
-
-    }
+    return {}
   },
-  methods: {
-
-  }
+  methods: {}
 }
 </script>
