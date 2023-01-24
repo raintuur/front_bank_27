12907a1818883296eb6e3f55f57b89fd11e428d7
Kuldar Nei <kuldarnei@gmail.com>
2023-01-24 14:59:34 +0200

    FEB-4 complete

---
+++ src/components/alert/AlertSuccess.vue
@@ -0,0 +1,13 @@
+<template>
+  <div v-if="message != ''" class="alert alert-success" role="alert">
+    {{ message }}
+  </div>
+</template>
+<script>
+export default {
+  name: 'AlertSuccess',
+  props: {
+    message: {}
+  }
+}
+</script>
--- src/views/AtmLocationView.vue
+++ src/views/AtmLocationView.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-
+    <AlertDanger :message="messageError"/>
+    <AlertSuccess :message="messageSuccess"/>
     <div class="row justify-content-center">
 
       <!-- COL 1   -->
@@ -13,7 +14,7 @@
         <LocationName @locationNameChangeEvent="setLocationName"/>
         <numberOfAtmsInput @numberOfAtmChangeEvent="setNumberOfAtms"/>
         <div>
-          <TransactionTypeCheckbox ref="transactionTypesRef" @transactionTypesUpdateEvent="setTransactionTypes"  />
+          <TransactionTypeCheckbox ref="transactionTypesRef" @transactionTypesUpdateEvent="setTransactionTypes"/>
         </div>
         <div>
           <image-input @pictureInputSuccess="setPictureBase64Data"/>
@@ -43,13 +44,22 @@ import LocationName from "@/components/atm/new/location_name/LocationName.vue";
 import NumberOfAtmsInput from "@/components/atm/new/numberOf/NumberOfAtmsInput.vue";
 import ImageInput from "@/components/ImageInput.vue";
 import atmsView from "@/views/AtmsView.vue";
+import AlertDanger from "@/components/alert/AlertDanger.vue";
+import AlertSuccess from "@/components/alert/AlertSuccess.vue";
+import {config} from "@fortawesome/fontawesome-svg-core";
 
 export default {
   name: "AtmLocationView",
-  components: {ImageInput, NumberOfAtmsInput, LocationName, CitiesDropdown, TransactionTypeCheckbox},
+  components: {
+    AlertSuccess,
+    AlertDanger, ImageInput, NumberOfAtmsInput, LocationName, CitiesDropdown, TransactionTypeCheckbox},
 
   data: function () {
     return {
+
+      messageError: '',
+      messageSuccess: '',
+
       locationName: '',
       numberOfAtms: 0,
 
@@ -76,15 +86,59 @@ export default {
     },
 
     addAtmLocation: function () {
-      // todo: käivitame checkbox info sissetõmbamise child komponendis (TransactionTypeCheckbox
       this.$refs.transactionTypesRef.sendTransactionTypesToParent()
 
-      this.$http.post("/atm/location", this.atmRequest
-      ).then(response => {
-        console.log('great success??')
+      // kontrollime ankeedi täitmist! (teeme eraldi meetodina :) )
+      console.log("add atm loc")
+      if (this.allRequiredFieldsFilled()) {
+        this.postAddAtmLocation()
+      } else {
+        this.messageError = 'Palun täitke kõik vajalikud väljad'
+      }
+    },
+
+    postAddAtmLocation: function () {
+      let preferExample = 'code=200'
+      if (this.atmRequest.locationName === 'Rimi') {
+        preferExample = 'code=403, example=403'
+      }
+
+      this.$http.post("/atm/location", this.atmRequest, {
+        headers: {
+          Prefer: preferExample
+        }
+      }).then(response => {
+        this.messageError = ''
+        this.messageSuccess = 'Uus ATM edukalt lisatud'
+        setTimeout(() => {
+          this.$router.go(0)
+        }, 700);
+
       }).catch(error => {
-        console.log(error)
+        this.messageError = error.response.data.errorMessage
       })
+    },
+
+    atLeastOneTransactionTypeSelected: function () {
+      let atLeastOneSelected = false
+      this.atmRequest.transactionTypes.forEach(element => {
+            if (element.isSelected) {
+              console.log('checkbox return true')
+              atLeastOneSelected = true
+            }
+          }
+      )
+      return atLeastOneSelected
+    },
+
+    allRequiredFieldsFilled: function () {
+      console.log("olen siin")
+      return (
+          this.atmRequest.cityId > 0 &&
+          this.atmRequest.locationName !== '' &&
+          this.atmRequest.numberOfAtms > 0 &&
+          this.atLeastOneTransactionTypeSelected()
+      )
     },
 
     setCityId: function (cityId) {
@@ -105,7 +159,7 @@ export default {
 
     navigateToAtms: function () {
       this.$router.push({name: 'atmsRoute'})
-    }
+    },
 
   }
 }
