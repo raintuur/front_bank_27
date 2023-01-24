2d5cff3e968890874e5d8c5e46dcf29dcf3babba
pbernotas <priit.bernotas@gmail.com>
2023-01-24 16:35:36 +0200

    FEB-4-COMPLETE

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,22 @@
 <template>
-<div>
+<div class="row justify-content-center">
 
+  <div class="col-4">
+    <form class="px-4 py-3">
+      <div class="mb-3">
+        <label class="form-label">Kasutajanimi</label>
+        <input v-model="username" type="text" class="form-control" placeholder="Mart123">
+      </div>
+      <div class="mb-3">
+        <label class="form-label">Parool</label>
+        <input v-model="password" type="password" class="form-control" placeholder="Parool">
+      </div>
+      <button v-on:click="login" type="submit" class="btn btn-primary">Logi sisse</button>
+    </form>
+    <div class="dropdown-divider"></div>
+    <a class="dropdown-item" href="#">New around here? Sign up</a>
+
+  </div>
 
 
 </div>
@@ -8,7 +24,37 @@
 
 <script>
 export default {
-  name: "LoginView"
+  name: "LoginView",
+  data: function () {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    login: function () {
+      this.$http.get("/login", {
+            params: {
+              username: this.username,
+              password: this.password
+            }
+          }
+      ).then(response => {
+        let userId = response.data.userId;
+        let roleType = response.data.roleType;
+
+        sessionStorage.setItem('userId', userId)
+        sessionStorage.setItem('roleType', roleType)
+        localStorage.setItem('lang', 'EST')
+
+        this.$router.push({name: 'atmsRoute'})
+
+        console.log(response.data)
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+  }
 }
 </script>
 
