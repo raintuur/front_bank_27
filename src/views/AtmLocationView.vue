e98d66802738065bdc8d830c504ea42316dafa6a
katrila <katrilattu@gmail.com>
2023-01-24 14:58:35 +0200

    KATRI-FL-2 complete

--- src/views/AtmLocationView.vue
+++ src/views/AtmLocationView.vue
@@ -146,6 +146,8 @@ export default {
       // kontrollime, etkas kõik vajalikud väljad on nõuetekohaselt täidetud
       if (this.allRequiredFieldsAreFilled()) {
         this.postAddAtmLocation();
+        setTimeout(() => {this.$router.go( 0)}, 2000
+        )
       } else {
         this.messageError = 'Täida kõik kohustuslikud väljad, vali ka vähemalt 1 teenus!'
       }
