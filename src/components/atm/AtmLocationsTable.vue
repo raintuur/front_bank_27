520d96d83fcbee16b64267815a39804a48f06f95
Valter Aro <valteraro@gmail.com>
2023-01-25 14:48:18 +0200

    VALTER-FEB-7 added stuff

--- src/components/atm/AtmLocationsTable.vue
+++ src/components/atm/AtmLocationsTable.vue
@@ -5,21 +5,33 @@
       <th scope="col">Linn</th>
       <th scope="col">Asukoht</th>
       <th scope="col">Teenused</th>
-      <th>nupp</th>
+      <th v-if="roleType === 'admin'">Muuda</th>
     </tr>
     </thead>
     <tbody>
 
     <tr v-for="atmLocation in atmLocations" :key="atmLocation.locationId">
       <td>{{ atmLocation.cityName }}</td>
-      <td>{{ atmLocation.locationName }}</td>
+      <td>
+        <div v-if="roleType === 'admin'">
+          <router-link :to="{name: 'editLocationRoute', query: {locationId:atmLocation.locationId}}">{{ atmLocation.locationName }} URL</router-link>
+          <br>
+          <router-link :to="{name: 'editLocationRoute', params: {locationId:atmLocation.locationId}}">{{ atmLocation.locationName }} PARAM</router-link>
+
+        </div>
+        <div v-else>
+          {{ atmLocation.locationName }}
+        </div>
+      </td>
       <td>
         <div v-for="transactionType in atmLocation.transactionTypes" :key="transactionType.typeName">
           {{ transactionType.typeName }}
         </div>
       </td>
-      <td><font-awesome-icon icon="fa-solid fa-pen-to-square" /></td>
-      <td><font-awesome-icon icon="fa-regular fa-pen-to-square" /></td>
+      <td v-if="roleType === 'admin'">
+        <font-awesome-icon v-on:click="navigateToEditAtmLocation(atmLocation.locationId)"
+                           icon="fa-regular fa-pen-to-square"/>
+      </td>
     </tr>
     </tbody>
   </table>
@@ -29,6 +41,7 @@ export default {
   name: 'AtmLocationsTable',
   data: function () {
     return {
+      roleType: sessionStorage.getItem('roleType'),
       atmLocations: [
         {
           locationId: 0,
@@ -60,6 +73,17 @@ export default {
         console.log(error)
       })
 
+    },
+    navigateToEditAtmLocation: function (locationId) {
+
+      this.$router.push({
+        name: 'editLocationRoute', query: {
+          locationId: locationId
+        }
+      })
+
+      // ToDo: navigeeri edit lehele, kaasa anna locationId
+      alert("location ID" + locationId)
     }
 
   },
---
+++ src/views/EditLocationView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    location id query/Request parameter (URL): {{locationId}}
+    location id parameter: {{locationIdAsParam}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EditLocationView",
+  data: function () {
+    return {
+      locationId: this.$route.query.locationId,
+      locationIdAsParam: this.$route.params.locationId
+    }
+  },
+  methods: {
+    getAtmLocation: function () {
+      this.$http.get("/atm/location", {
+            params: {
+              locationId: this.locationId,
+            }
+          }
+      ).then(response => {
+        console.log(response.data)
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+  },
+  beforeMount() {
+    alert(this.getAtmLocation)
+  }
+}
+</script>
+
