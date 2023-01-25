91eede081557ef06eb2619904b9d8b3f4f2104ce
Janeli <janelikurm@gmail.com>
2023-01-25 14:49:53 +0200

    FEB 7 - muuda veerg lisatud

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
 <!--      <router-link to="/libraries">Raamatukogud</router-link> |-->
       <router-link to="/atms">Pangaautomaadid</router-link> |
       <router-link to="/atm/location">Lisa uus asukoht</router-link> |
+      <router-link to="/admin/edit/location">Muuda asukoha infot</router-link> |
       <router-link to="/login">Sisse logimine</router-link>
 
     </nav>
