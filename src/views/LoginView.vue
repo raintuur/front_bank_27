26aeaa02963a526f7d63c8df085b9df20a00d385
Helar Kärbo <helar.karbo@gmail.com>
2023-01-31 12:23:54 +0200

    HELAR-FEB-7 we have all changed

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,7 +3,7 @@
 
 
     <div class="col-4 ">
-
+      <AlertDanger :message="message"/>
 
       <form class="px-4 py-3">
         <div class="mb-3">
@@ -25,11 +25,14 @@
 </template>
 
 <script>
+import AlertDanger from "@/components/alert/AlertDanger.vue";
+
 export default {
   name: "LoginView",
+  components: {AlertDanger},
   data: function () {
     return {
-
+      message: '',
       loginResponse: {
         userId: 0,
         roleType: ''
@@ -48,6 +51,15 @@ export default {
   methods: {
 
     login: function () {
+      this.message = ''
+      if (this.username == '' || this.password == '') {
+        this.message = 'Täida kõik väljad!';
+      } else {
+        this.sendLoginRequest
+      }
+      this.sendLoginRequest();
+    },
+    sendLoginRequest: function () {
       this.$http.get("/login", {
             params: {
               username: this.username,
@@ -64,9 +76,10 @@ export default {
         this.$router.push({name: 'atmsRoute'})
 
       }).catch(error => {
-        console.log(error)
-      })
-    },
+        this.apiError = error.response.data
+        this.message = this.apiError.message
+      });
+    }
   }
 }
 </script>
