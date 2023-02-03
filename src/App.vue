df35b7c36702a1ea0a2048a4c67d8a8e20b6ed62
Janeli <janelikurm@gmail.com>
2023-02-03 13:01:26 +0200

    working

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,41 @@
     <nav>
       <router-link to="/"><font-awesome-icon icon="fa-solid fa-house-chimney" /></router-link> |
       <router-link to="/atms">Pangaautomaadid</router-link> |
-      <router-link :to="{name: 'atmLocationRoute', query: {isAdd:'true'}}">Asukoht</router-link> |
-      <router-link to="/login">Sisse logimine</router-link>
+      <router-link v-if="isAdmin" :to="{name: 'atmLocationRoute', query: {isAdd:'true'}}">Asukoht | </router-link>
+      <router-link v-if="userLoggedIn" to="/">Logi välja</router-link>
+      <router-link v-else to="/login">Logi sisse</router-link>
     </nav>
-    <router-view/>
+    <router-view @emitLoginSuccessEvent="updateNavigationMenu"/>
   </div>
 </template>
+<script>
+export default {
+  name: "App",
+  data: function () {
+    return {
+      userLoggedIn: false,
+      isAdmin: false,
+      userId: sessionStorage.getItem('userId'),
+      roleType: sessionStorage.getItem('roleType')
+    }
+  },
+  methods: {
+    updateNavigationMenu: function () {
+      this.userId = sessionStorage.getItem('userId')
+      this.roleType = sessionStorage.getItem('roleType')
+      // kui userId on tühi, siis userLoggedIn = false
+      // kui userId on täidetud, siis userLoggedIn = true
+      this.userLoggedIn = this.userId != null
 
+      this.isAdmin = this.roleType === 'admin'
+
+    }
+  },
+  mounted() {
+    this.updateNavigationMenu()
+  }
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
